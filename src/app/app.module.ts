069c0aae885bc97772d963cd0d4f2e6f40f1d19c
Tobias Karzewski <tobias@karzewski.de>
2020-04-15 10:00:51 +0200

    minor changes to publish to npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-// import { KatschangularThreeStatesToggleModule } from 'katschangular-three-states-toggle';
 import { MatSliderModule } from '@angular/material/slider';
-import { KatschangularThreeStatesToggleModule } from '../../projects/katschangular-three-states-toggle/src/lib/katschangular-three-states-toggle.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+import { KatschangularThreeStatesToggleModule } from 'katschangular-three-states-toggle';
+// import { KatschangularThreeStatesToggleModule }
+// from
+// '../../projects/katschangular-three-states-toggle/src/lib/katschangular-three-states-toggle.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
